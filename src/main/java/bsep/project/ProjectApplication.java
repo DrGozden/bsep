30fccd36b2e07cd9bd68c1da54de8f43e0765fca
DrGozden <gozden123@gmail.com>
2020-04-14 17:05:23 +0200

    prepravka

--- src/main/java/bsep/project/ProjectApplication.java
+++ src/main/java/bsep/project/ProjectApplication.java
@@ -3,10 +3,17 @@ package bsep.project;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.security.Security;
+
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 @SpringBootApplication
 public class ProjectApplication {
 
 	public static void main(String[] args) {
+		Security.addProvider(new BouncyCastleProvider());
 		SpringApplication.run(ProjectApplication.class, args);
 	}
 
